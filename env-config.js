ec491bbc0da573a1253b74367ea1b63578978929
endziu <koper.andrzej@gmail.com>
2017-07-31 09:55:58 +0200

    update ip

--- env-config.js
+++ env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BACKEND_URL': prod
-    ? 'http://37.233.102.24:3000'
+    ? 'http://178.62.249.30:3000/'
     : 'http://localhost:3000'
 };
