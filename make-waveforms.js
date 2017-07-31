f7494158802c3fa38eb1acfb23e80ab8d739d4d5
endziu <koper.andrzej@gmail.com>
2017-07-31 21:22:05 +0200

    formating

--- make-waveforms.js
+++ make-waveforms.js
@@ -1,22 +1,21 @@
 var Waveform = require('node-wave');
 var fs = require('fs');
 
-const files = fs.readdirSync('./public/sounds')
+const files = fs.readdirSync('./public/sounds');
 
-const options = 
-  { width: 768
-  , height: 64
-  , baseline: 32
-  , padding:2
-  , waveColor: 'white',backgroundColor: 'rgba(0,0,0,0)'
-  }
+const options = {
+  width: 1024,
+  height: 64,
+  baseline: 32,
+  padding: 2,
+  waveColor: 'white',
+  backgroundColor: 'rgba(0,0,0,0)'
+};
 
 files.forEach((file, i, arr) => {
-  Waveform(
-    `./public/sounds/${file}`,
-    options,
-    (err, buffer) => fs.writeFileSync(`./public/waves/${file.replace('.mp3', '.png')}`, buffer)
-  )
-})
+  Waveform(`./public/sounds/${file}`, options, (err, buffer) =>
+    fs.writeFileSync(`./public/waves/${file.replace('.mp3', '.png')}`, buffer)
+  );
+});
 
-console.log('done!')
+console.log('done!');
