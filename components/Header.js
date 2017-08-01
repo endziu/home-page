acef661d4391b99622562ae0c6a7cae63d90e4ac
endziu <koper.andrzej@gmail.com>
2017-08-01 15:48:44 +0200

    nav

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,21 @@ export default () => (
   <header className="pv1 pl2 bg-navy mw8 center bb bw1 b--green">
     <nav className="f3-ns f4 flex items-center">
       <Link href="/">
-        <a className={'white f3 no-underline mr3 hover-green'}>music</a>
+        <a className={'white f3 no-underline mr3 hover-blue'}>endziu.xyz</a>
       </Link>
-      <Link href="/info">
-        <a className={'white f3 no-underline mr3 hover-yellow'}>info</a>
+      <Link href="/music">
+        <a className={'white f4 no-underline mr3 hover-green'}>music</a>
+      </Link>
+      <Link href="/code">
+        <a className={'white f4 no-underline mr3 hover-yellow'}>code</a>
+      </Link>
+      <Link>
+        <a
+          className={'white f4 no-underline mr3 hover-orange'}
+          href="https://dzwiekowamapa.pl"
+        >
+          soundMap
+        </a>
       </Link>
     </nav>
   </header>
---
+++ pages/code.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout.js';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+  <Layout>
+    <pre className="pl2 white mw8 center">
+      coding coding  00 01 10...
+    </pre>
+  </Layout>
+);
+
+export default Index;
