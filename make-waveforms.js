295ccbc95e9c6033527c5684e2422259e2b5e6ee
endziu <koper.andrzej@gmail.com>
2017-08-02 13:56:38 +0200

    rm sounds

--- make-waveforms.js
+++ make-waveforms.js
@@ -12,9 +12,12 @@ const options = {
   backgroundColor: 'rgba(0,0,0,0)'
 };
 
+console.log(files)
+
+
+
 files.forEach((file, i, arr) => {
-  Waveform(`./public/sounds/${file}`, options, (err, buffer) =>
+  Waveform(`./public/sounds/${file}`, options, (err, buffer) => {
     fs.writeFileSync(`./public/waves/${file.replace('.mp3', '.png')}`, buffer)
-  );
-  console.log(`${i + 1} done! ${arr.length - i} to go`);
-});
+  })
+})
