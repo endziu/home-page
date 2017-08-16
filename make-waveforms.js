7e71390e55063a4fd8b70e853ea5884c973b02d5
endziu <koper.andrzej@gmail.com>
2017-08-16 12:09:11 +0200

    waveforms

--- make-waveforms.js
+++ make-waveforms.js
@@ -1,19 +1,21 @@
 const { spawn } = require('child_process')
 const sounds = require('./db/sounds.json')
 
-console.log(sounds)
-
 const ffmpeg = (sound, i, arr) => {
-  spawn('ffmpeg', [
-    '-y',
-    '-i',
-    sound.sound_url,
-    '-filter_complex',
-    'showwavespic=s=1024x120:colors=white',
-    '-frames:v',
-    '1',
-    sound.waveform_url
-  ])
+  spawn(
+    'ffmpeg',
+    [
+      '-y',
+      '-i',
+      sound.sound_url,
+      '-filter_complex',
+      'showwavespic=s=1024x120:colors=white',
+      '-frames:v',
+      '1',
+      sound.waveform_url
+    ],
+    console.log(sound)
+  )
 }
 
 sounds.map(ffmpeg)
