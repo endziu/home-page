017370f7e4e336c02bd6259180e7f94f4ef01a19
endziu <koper.andrzej@gmail.com>
2017-08-23 16:22:58 +0200

    style

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default () => (
   <footer className="fixed bottom-0 right-0 pa1">
     <a
       href="https://endziu.xyz"
-      className="f6 white no-underline underline-hover"
+      className="f6 black no-underline underline-hover"
     >
       ©
     </a>
