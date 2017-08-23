874baeb397c253f38dca9c6539e1df030a2e6e47
endziu <koper.andrzej@gmail.com>
2017-08-23 11:47:12 +0200

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,93 +1,82 @@
 import Layout from '../components/Layout.js'
 import SvgLink from '../components/SvgLink.js'
 
+const Line = ({ text, href, link }) => (
+  <p className="mv1">
+    {text}
+    <a target="_blank" href={href} className="link blue dim no-undeline">
+      {link}
+    </a>
+  </p>
+)
+
+const Block = props => (
+  <div className="measure">
+    <hr />
+    {props.children}
+  </div>
+)
+
 const Index = props => (
   <Layout path={props.url.pathname}>
     <div className="pl2 white mw8 center">
-      <h2 className="f2 f1-m f-headline-l mv2 mv3-ns ml0 mh0">
-        {`hello, I'm Andrzej`}
-      </h2>
-      <div className="measure pa0">
+      <header>
+        <h2 className="f2 f1-m f-headline-l mv2 mv3-ns ml0 mh0">
+          {`hello, I'm Andrzej`}
+        </h2>
         <h2 className="ma0 f4 f3-ns mb4-l mb3-m mb2">
           {`I make music, sounds & JavaScript.`}
         </h2>
-        <hr />
-        <p className="mv1">
-          {`code I write is open source on `}
-          <a
-            target="_blank"
-            href="https://github.com/endziu"
-            className="link blue dim no-underline"
-          >
-            github
-          </a>
-          {'.'}
-        </p>
-        <p className="mv1">
-          {`I post music online as `}
-          <a
-            target="_blank"
-            href="https://soundcloud.com/abstract-machines"
-            className="link blue dim no-underline"
-          >
-            @abstract-machines
-          </a>
-        </p>
-        <p className="mv1">
-          {`and I also keep some music over `}
-          <a href="/music" className="link blue dim no-underline">
-            here
-          </a>
-          {'. '}
-        </p>
-        <p className="mv1">
-          {`you can follow me on twitter `}
-          <a
-            target="_blank"
-            href="https://twitter.com/1000machines"
-            className="link blue dim no-underline"
-          >
-            @1000machines
-          </a>
-          {'. '}
-        </p>
-        <div className="measure">
-          <hr />
-          <p className="mv1">
-            I support cryptocurrencies:
-          </p>
-          <p className="mv1">
-            {`bitcoin: `}
-            <a
-              href="/imgs/btc-qr-code.png"
-              className="link blue dim no-underline"
-            >
-              1qa4xA4DXR26oPhXXAvy9ivByDYXkmhpU
-            </a>
-          </p>
-          <p className="mv1">
-            {`ethereum: `}
-            <a
-              href="/imgs/eth-qr-code.png"
-              className="link blue dim no-underline"
-            >
-              0xc3adab50c47b3d9c3342d71202838ddb5f09a050
-            </a>
-          </p>
+      </header>
+      <Block>
+        <div>
+          <Line
+            text={`code I write is open source on `}
+            href={`https://github.com/endziu`}
+            link={`github`}
+          />
+          <Line
+            text={`I post music online as `}
+            href={`https://soundcloud.com/abstract-machines`}
+            link={`@abstract-machines`}
+          />
+          <Line
+            text={`and I also keep some music over `}
+            href={`/music`}
+            link={`here`}
+          />
+          <Line
+            text={`you can follow me on twitter `}
+            href={`https://twitter.com/1000machines`}
+            link={`@1000machines`}
+          />
         </div>
-        <div className="measure-narrow">
-          <hr />
-          <p className="mv1">
-            {`feel free to send me an `}
-            <a
-              href="mailto:koper.andzrzej@gmail.com"
-              className="link blue dim no-underline"
-            >
-              email
-            </a>
-          </p>
+      </Block>
+
+      <Block>
+        <div>
+          <Line text={`I support cryptocurrencies:`} />
+          <Line
+            text={`bitcoin: `}
+            href={`/imgs/btc-qr-code.png`}
+            link={`1qa4xA4DXR26oPhXXAvy9ivByDYXkmhpU`}
+          />
+          <Line
+            text={`ethereum: `}
+            href={`/imgs/eth-qr-code.png`}
+            link={`0xc3adab50c47b3d9c3342d71202838ddb5f09a050`}
+          />
         </div>
-      </div>
+      </Block>
+
+      <Block>
+        <Line
+          text={`feel free to send me an `}
+          href={`mailto:koper.andzrzej@gmail.com`}
+          link={`email`}
+        />
+      </Block>
+
     </div>
   </Layout>
 )
