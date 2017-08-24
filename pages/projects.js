0fc125c31c279ab88f79b61ae87f493eb143a20d
endziu <koper.andrzej@gmail.com>
2017-08-24 18:36:39 +0200

    fix link's background

--- pages/projects.js
+++ pages/projects.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout.js'
 
 const Index = props => (
   <Layout path={props.url.pathname}>
-    <div className="pl2 mw8 black-80 center">
+    <div className="mw8 black-80 center pl2">
       <h2>
         web development
       </h2>
@@ -11,7 +11,7 @@ const Index = props => (
           <a
             target="_blank"
             href="https://dzwiekowamapa.pl"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             dzwiekowaMapa.pl
           </a>
@@ -20,85 +20,21 @@ const Index = props => (
           <a
             target="_blank"
             href="https://endziu.xyz"
-            className="link pl2 black-80 underline-hover hover-red"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             old home page
           </a>
         </li>
       </ul>
       <h2>
-        music recording
-      </h2>
-      <ul className="list pa0">
-        <li>
-          <a
-            target="_blank"
-            href="https://mombe.bandcamp.com/album/momb-2"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
-          >
-            mombe
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://thejazzstatues.bandcamp.com/album/the-jazz-statues-conquer"
-            className="link pl2 black-80 underline-hover hover-red"
-          >
-            the jazz statues
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://www.youtube.com/watch?v=S_WFWuoMEEk"
-            className="link pl2 black-80 underline-hover hover-red"
-          >
-            sewa kaffo
-          </a>
-        </li>
-      </ul>
-      <h2>
-        music for videos
-      </h2>
-      <ul className="list pa0">
-        <li>
-          <a
-            target="_blank"
-            href="https://vimeo.com/85443642"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
-          >
-            micro situations
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://www.youtube.com/watch?v=DgTGhln-rlM"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
-          >
-            ucieczka ze szklanej kuli
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://vimeo.com/11533775"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
-          >
-            ty i rys społeczny
-          </a>
-        </li>
-      </ul>
-      <h2>
-        sound & music for movies
+        music production & sound design
       </h2>
       <ul className="list pa0">
         <li>
           <a
             target="_blank"
             href="http://www.filmpolski.pl/fp/index.php?film=1217848"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             rzeźnia numer 1
           </a>
@@ -107,7 +43,7 @@ const Index = props => (
           <a
             target="_blank"
             href="http://www.filmpolski.pl/fp/index.php?film=1216732"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             krótka histeria czasu
           </a>
@@ -116,7 +52,7 @@ const Index = props => (
           <a
             target="_blank"
             href="http://www.filmpolski.pl/fp/index.php?film=1214406"
-            className="link pl2 black-80 underline-hover hover-red"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             krew z nosa
           </a>
@@ -125,7 +61,7 @@ const Index = props => (
           <a
             target="_blank"
             href="http://www.filmpolski.pl/fp/index.php?film=4224943"
-            className="link pl2 black-80 underline-hover hover-red"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             pokój
           </a>
@@ -134,21 +70,43 @@ const Index = props => (
           <a
             target="_blank"
             href="http://www.filmpolski.pl/fp/index.php?film=1221991"
-            className="link pl2 black-80 underline-hover hover-red"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             czarny
           </a>
         </li>
-      </ul>
-      <h2>
-        music for tv & ads
-      </h2>
-      <ul className="list pa0">
+        <li>
+          <a
+            target="_blank"
+            href="https://vimeo.com/85443642"
+            className="link black-80 underline-hover hover-red bg-white-80"
+          >
+            micro situations
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.youtube.com/watch?v=DgTGhln-rlM"
+            className="link black-80 underline-hover hover-red bg-white-80"
+          >
+            ucieczka ze szklanej kuli
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://vimeo.com/11533775"
+            className="link black-80 underline-hover hover-red bg-white-80"
+          >
+            ty i rys społeczny
+          </a>
+        </li>
         <li>
           <a
             target="_blank"
             href="https://vimeo.com/185946610"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             ArC2
           </a>
@@ -157,12 +115,46 @@ const Index = props => (
           <a
             target="_blank"
             href="https://www.atmgrupa.pl/produkcja/gra-w-ciemno/"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             gra w ciemno
           </a>
         </li>
       </ul>
+
+      <h2>
+        music recording
+      </h2>
+      <ul className="list pa0">
+        <li>
+          <a
+            target="_blank"
+            href="https://mombe.bandcamp.com/album/momb-2"
+            className="link black-80 underline-hover hover-red bg-white-80"
+          >
+            mombe
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://thejazzstatues.bandcamp.com/album/the-jazz-statues-conquer"
+            className="link black-80 underline-hover hover-red bg-white-80"
+          >
+            the jazz statues
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.youtube.com/watch?v=S_WFWuoMEEk"
+            className="link black-80 underline-hover hover-red bg-white-80"
+          >
+            sewa kaffo
+          </a>
+        </li>
+      </ul>
+
       <h2>
         AV performances
       </h2>
@@ -171,7 +163,7 @@ const Index = props => (
           <a
             target="_blank"
             href="https://vimeo.com/123825272"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             live @ceta
           </a>
@@ -180,10 +172,15 @@ const Index = props => (
           <a
             target="_blank"
             href="https://www.youtube.com/watch?v=ZpCya0Pj47E"
-            className="link pl2 black-80 underline-hover hover-red bg-white-80"
+            className="link black-80 underline-hover hover-red bg-white-80"
           >
             live @Puzzle
           </a>
+        </li>
+        <li>
+          <span className="link black-80 underline-hover hover-red bg-white-80">
+            live @Login
+          </span>
         </li>
       </ul>
     </div>
