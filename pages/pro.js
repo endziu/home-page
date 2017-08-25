841a04dcf79a40708436b8b4d5ad61b2bb3d5ec3
endziu <koper.andrzej@gmail.com>
2017-08-25 17:28:50 +0200

    refactor common bits

--- pages/pro.js
+++ pages/pro.js
@@ -1,152 +1,34 @@
 import Layout from '../components/Layout.js'
 
+const Section = ({ imgUrl, type }) => (
+  <section className="cf w-100 pa2-ns mw8 center">
+    <h1>{type}</h1>
+    <article className="fl w-25 w-20-m w-10-l pa2">
+      <div className="aspect-ratio aspect-ratio--1x1">
+        <img
+          style={{
+            backgroundImage: `url(${imgUrl})`
+          }}
+          className="db bg-center cover aspect-ratio--object"
+        />
+        <a href="#0" className="absolute link z-999">
+          <h3 className="f6 black-80 bg-white-60">Title of piece</h3>
+        </a>
+      </div>
+    </article>
+  </section>
+)
+
 const Index = props => (
   <Layout path={props.url.pathname}>
-    <section className="cf w-100 pa2-ns mw8 center">
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0006.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0006.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0006.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0002.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-      <article className="fl w-25 w-20-m w-10-l pa2 hide-child">
-        <div className="aspect-ratio aspect-ratio--1x1">
-          <img
-            style={{
-              backgroundImage: 'url(http://mrmrs.github.io/images/0006.jpg)'
-            }}
-            className="db bg-center cover aspect-ratio--object grow"
-          />
-        </div>
-        <a href="#0" className="ph2 ph0-ns pb3 link db child">
-          <h3 className="f6 mb0 black-90">Title of piece</h3>
-        </a>
-      </article>
-    </section>
+    <Section
+      type="web development"
+      imgUrl="http://mrmrs.github.io/images/0006.jpg"
+    />
+    <Section
+      type="music production"
+      imgUrl="http://mrmrs.github.io/images/0006.jpg"
+    />
   </Layout>
 )
 
