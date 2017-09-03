f5ceb99c4c713fddf7f533961962a3fc2faea5b1
endziu <koper.andrzej@gmail.com>
2017-09-03 14:25:28 +0200

    fixin

--- env-config.js
+++ env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
   'process.env.BACKEND_URL': prod
-    ? 'https://178.62.249.30:16662'
-    : 'https://localhost:16662'
+    ? 'http://178.62.249.30:16662'
+    : 'http://localhost:16662'
 }
