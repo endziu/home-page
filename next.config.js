cbc03cb1b94e28aa9ac758a8e0ab7af33bbafddd
endziu <koper.andrzej@gmail.com>
2018-06-04 19:25:22 +0200

    fix backend address

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const isProd = process.env.NODE_ENV === "production"
 
 module.exports = {
     serverRuntimeConfig: { // Will only be available on the server side
-      backEndUrl: isProd ? 'https://endziu.xyz:16662' : 'http://localhost:16662'
+      backEndUrl: isProd ? 'https://endziu.xyz' : 'http://localhost:16662'
     },
     publicRuntimeConfig: { // Will be available on both server and client
-      backEndUrl: isProd ? 'https://endziu.xyz:16662' : 'http://localhost:16662'
+      backEndUrl: isProd ? 'https://endziu.xyz' : 'http://localhost:16662'
     }
   }
