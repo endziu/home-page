a5e231e3adc00843013894c8bb6075af6f785b9f
endziu <koper.andrzej@gmail.com>
2020-06-21 01:03:31 +0200

    update

--- db/projectsInfo.js
+++ db/projectsInfo.js
@@ -1,98 +1,103 @@
 const webdev = [
   {
+    imgUrl: '/imgs/matejko.jpg',
+    name: 'matejko.site',
+    link: 'https://matejko.site',
+  },
+  {
     imgUrl: '/imgs/mapa.jpg',
     name: 'dźwiekowa mapa',
-    link: 'https://dzwiekowamapa.pl'
+    link: 'https://dzwiekowamapa.pl',
   },
   {
     imgUrl: '/imgs/canvas.jpg',
     name: 'canvas physics',
-    link: '/canvas'
-  }
+    link: '/canvas',
+  },
 ]
 
 const rec = [
   {
     imgUrl: '/imgs/mombe.jpg',
     name: 'mombe',
-    link: 'https://mombe.bandcamp.com/album/momb-2'
+    link: 'https://mombe.bandcamp.com/album/momb-2',
   },
   {
     imgUrl: '/imgs/statues.jpg',
     name: 'the jazz statues',
-    link: 'https://thejazzstatues.bandcamp.com/album/the-jazz-statues-conquer'
+    link: 'https://thejazzstatues.bandcamp.com/album/the-jazz-statues-conquer',
   },
   {
     imgUrl: '/imgs/alinga.jpg',
     name: 'sewa kafo',
-    link: 'https://www.youtube.com/watch?v=S_WFWuoMEEk'
-  }
+    link: 'https://www.youtube.com/watch?v=S_WFWuoMEEk',
+  },
 ]
 
 const av = [
   {
     imgUrl: '/imgs/ceta.jpg',
     name: 'live @ceta',
-    link: 'https://www.youtube.com/watch?v=g2H8L6UeEh8'
+    link: 'https://www.youtube.com/watch?v=g2H8L6UeEh8',
   },
   {
     imgUrl: '/imgs/puzzle.jpg',
     name: 'live @Puzzle',
-    link: 'https://www.youtube.com/watch?v=ZpCya0Pj47E'
+    link: 'https://www.youtube.com/watch?v=ZpCya0Pj47E',
   },
   {
     imgUrl: '/imgs/login.jpg',
     name: 'live @Login',
-    link: ''
-  }
+    link: '',
+  },
 ]
 
 const prod = [
   {
     imgUrl: '/imgs/arc.jpg',
     name: 'ArC2',
-    link: 'https://vimeo.com/185946610'
+    link: 'https://vimeo.com/185946610',
   },
   {
     imgUrl: '/imgs/micro.jpg',
     name: 'micro situations',
-    link: 'https://vimeo.com/85443642'
+    link: 'https://vimeo.com/85443642',
   },
   {
     imgUrl: '/imgs/ucieczka.jpg',
     name: 'ucieczka ze szklanej kuli',
-    link: 'https://www.youtube.com/watch?v=DgTGhln-rlM'
+    link: 'https://www.youtube.com/watch?v=DgTGhln-rlM',
   },
   {
     imgUrl: '/imgs/rys.jpg',
     name: 'ty i rys społeczny',
-    link: 'https://vimeo.com/11533775'
+    link: 'https://vimeo.com/11533775',
   },
   {
     imgUrl: '/imgs/pokoj.jpg',
     name: 'pokój',
-    link: 'http://www.filmpolski.pl/fp/index.php?film=4224943'
+    link: 'http://www.filmpolski.pl/fp/index.php?film=4224943',
   },
   {
     imgUrl: '/imgs/rzeznia.jpg',
     name: 'rzeźnia numer 1',
-    link: 'http://www.filmpolski.pl/fp/index.php?film=1217848'
+    link: 'http://www.filmpolski.pl/fp/index.php?film=1217848',
   },
   {
     imgUrl: '/imgs/histeria.jpg',
     name: 'krótka histeria czasu',
-    link: 'http://www.filmpolski.pl/fp/index.php?film=1216732'
+    link: 'http://www.filmpolski.pl/fp/index.php?film=1216732',
   },
   {
     imgUrl: '/imgs/gra.jpg',
     name: 'gra w ciemno',
-    link: 'https://www.atmgrupa.pl/produkcja/gra-w-ciemno/'
+    link: 'https://www.atmgrupa.pl/produkcja/gra-w-ciemno/',
   },
   {
     imgUrl: '/imgs/krew.jpg',
     name: 'krew z nosa',
-    link: 'http://www.filmpolski.pl/fp/index.php?film=1214406'
-  }
+    link: 'http://www.filmpolski.pl/fp/index.php?film=1214406',
+  },
 ]
 
 export { webdev, rec, av, prod }
